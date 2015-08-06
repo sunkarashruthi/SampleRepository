96692b5e9aadde63e31057570d48955df1382e15
sunkarashruhti <shruthichowdary17@gmail.com>
2015-08-06 12:55:47 -0700

    js

---
+++ addition.js
@@ -0,0 +1,9 @@
+function add()
+{
+	var a = document.getElementById('first_number').value;
+var b= document.getElementById('second_number').value;
+	
+var c= parseInt(a)+parseInt(b);
+alert(c);
+
+}
